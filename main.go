df68e60c026a0ef091dc8d15a19a5e39dd69b686
Arad bin <arad.siddiqui@upaybd.com>
2024-04-17 15:30:47 +0600

    go init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// database.DatabaseInit()
+	// gorm := database.DB()
+	// gorm.AutoMigrate(&blog.Blog{})
+
+	e := echo.New()
+	// blog.BlogRoutes(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
